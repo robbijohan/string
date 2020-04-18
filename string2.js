f2dadc041d52aaa90ee45982e85ff517d89f1266
robbijohan <gawemeneh8@gmail.com>
2020-04-18 16:35:16 +0700

    Tugas String

---
+++ string2.js
@@ -0,0 +1,16 @@
+var word = 'wow JavaScript is so cool';
+
+var FirstWord = word[0] + word[1] + word[2];
+console.log('First Word: ' + FirstWord);
+
+var secondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] +word[13];
+console.log('Second Word: ' + secondWord);
+
+var thirdWord = word[15] + word[16];
+console.log('Third Word: ' + thirdWord);
+
+var fourthWord = word[18] + word[19];
+console.log('Fourth Word: ' + fourthWord);
+
+var fifthWord = word[21] + word[22] + word[23] + word[24]
+console.log('FifthWord: ' + fifthWord);
